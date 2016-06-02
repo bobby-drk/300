08a867ab650e423d41fede33061e8f963e445667
Charlie King <charlie.king@clearlink.com>
2016-06-02 00:52:58 -0600

    fake some data

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,11 +11,35 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(App\Models\User::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->safeEmail,
-        'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'api_token' => $faker->swiftBicNumber,
+    ];
+});
+
+
+$factory->define(App\Models\Ledger::class, function (Faker\Generator $faker) {
+
+    $users_as_obj = DB::table('users')->select('id')->get();
+
+    foreach($users_as_obj as $obj) {
+        $users[] = $obj->id;
+    }
+
+    $creditor = $faker->randomElement($users);
+
+    if(($key = array_search($creditor, $users)) !== false) {
+        unset($users[$key]);
+    }
+
+    $debtor = $faker->randomElement($users);
+
+    return [
+        'creditor' => $creditor,
+        'debtor' => $debtor,
+        'credit_type' => $faker->randomElement(['food', 'bowling']),
+        'amount' => $faker->randomFloat(2, 1, 8),
     ];
 });
