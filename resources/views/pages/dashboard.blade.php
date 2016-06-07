558f5ea797b6903c2862ff19fcada1643edc62d7
Charlie King <charlie.king@clearlink.com>
2016-06-06 20:12:25 -0600

    no message

---
+++ resources/views/pages/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.default')
+
+@section('page-title')
+    Dashboard
+@stop
+
+@section('content')
+    <div class="row">
+        <div>Balance</div>
+        <div>PR</div>
+        <div>Bowling Scores over time graph</div>
+    </div>
+@stop
