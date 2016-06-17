5a5e219d03995234713aed0360dc33fb588c39ca
Charlie King <charlie.king@clearlink.com>
2016-06-16 20:02:52 -0700

    give the home page some style

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,7 +5,7 @@
 @stop
 
 @section('content')
-    <div class="row weak_small_boarder">
+    <div class="row weak_small_boarder fancy_bg">
 
         <div class='col-md-4 vertical_offset_s'><a href="{{ route("scores") }}" class="btn btn-default btn-lg btn-block">Record score</a></div>
         <div class='col-md-4 vertical_offset_s'><a href="{{ route("record_food") }}" class="btn btn-default btn-lg btn-block">I paid for food</a></div>
