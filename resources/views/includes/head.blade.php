acf557ffd857e2ee736dbe9c8b4323c71dca8f80
Charlie King <charlie.king@clearlink.com>
2016-06-22 07:41:47 -0600

    app_icon

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,6 +1,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="apple-touch-icon" href="{{ URL::asset('assets/images/app_icon.png')}}"/>
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link rel="icon" type="image/ico" href="/favicon.ico"/>
