f3a539f7f4cf7fad05138fd43f59b6b932d68851
Jason Wilkin <Jason Wilkin>
2016-06-27 15:55:04 -0600

    Charts....the beginning!

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -4,6 +4,50 @@
     Dashboard
 @stop
 
+@push('page-js')
+    <script src="{{ URL::asset('assets/js/Chart.js') }}"></script>
+    <script>
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+</script>
+@endpush()
+
+
 @section('content')
 <!--    <div class="row well strong_small_boarder">-->
         <div class="panel panel-info">
@@ -60,8 +104,13 @@
                 <br />
                 <div>
                     Bowling Scores over time graph
+                    
+                    <canvas id="myChart" width="400" height="400"></canvas>
+
                 </div>
             </div>
         </div>
+
+
     <!--</div>-->
 @stop
